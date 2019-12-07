73f97c57ec3d3aafa05fff092b80b0bdd65f99d8
23michael45 <michael2345@live.cn>
2019-12-07 15:17:35 +0800

    add public

--- YSurgeryUnityDemo/app/src/main/java/com/yuji/face/YSurgeryUnityInterface.java
+++ YSurgeryUnityDemo/app/src/main/java/com/yuji/face/YSurgeryUnityInterface.java
@@ -50,7 +50,7 @@ public class YSurgeryUnityInterface {
     //description : 服务器计算的高模转成低模，生成roleJson数据结构
     //param1 ：hdObjData  服务机计算生成的obj在本地磁盘上的文件二进制bytes数据
     //return :  json字符串，roleJson数据结构
-    String CalculateLowPolyFace(byte[] hdObjData)
+    public String CalculateLowPolyFace(byte[] hdObjData)
     {
         return unityListener.onCalculateLowPolyFace(new BytesWrapper(hdObjData));
     }
@@ -59,7 +59,7 @@ public class YSurgeryUnityInterface {
     //param1 ： 计算生成的低模json数据字符串
     //param2 ： 服务机计算生成的贴图jpg二进制bytes
     //return :  读取成功或失败
-    boolean LoadLowPolyFace(String roleJson,byte[] textureData)
+    public boolean LoadLowPolyFace(String roleJson,byte[] textureData)
     {
         return unityListener.onLoadLowPolyFace(roleJson,new BytesWrapper(textureData));
     }
@@ -67,7 +67,7 @@ public class YSurgeryUnityInterface {
 
     //description : 保存当前模型变型数据到deformJson数据结构
     //return :  json字符串，deformJson数据
-    String SaveDeform()
+    public String SaveDeform()
     {
         return unityListener.onSaveDeform();
     }
@@ -76,7 +76,7 @@ public class YSurgeryUnityInterface {
     //description : 读取deformJson数据结构,读取到当前模型
     //param1 ： 低模变形deformJson数据字符串
     //return :  读取成功或失败
-    boolean LoadDeform(String deformJson)
+    public boolean LoadDeform(String deformJson)
     {
         return unityListener.onLoadDeform(deformJson);
 
@@ -84,7 +84,7 @@ public class YSurgeryUnityInterface {
 
     //description : 保存当前模型换状数据到avatarJson数据结构
     //return :  json字符串，avatarJson数据
-    String SaveAvatar()
+    public String SaveAvatar()
     {
         return unityListener.onSaveAvatar();
 
@@ -93,7 +93,7 @@ public class YSurgeryUnityInterface {
     //description : 读取avatarJson数据结构,读取到当前模型
     //param1 ： 低模换装avatarJson数据字符串
     //return :  读取成功或失败
-    boolean LoadAvatar(String avatarJson)
+    public boolean LoadAvatar(String avatarJson)
     {
 
         return unityListener.onLoadAvatar(avatarJson);
