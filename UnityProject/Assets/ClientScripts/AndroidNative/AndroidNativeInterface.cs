b57c14c25cc9c43eec864366fd351ab3f2b22328
23michael45 <michael2345@live.cn>
2019-12-16 14:52:02 +0800

    interface add

--- UnityProject/Assets/ClientScripts/AndroidNative/AndroidNativeInterface.cs
+++ UnityProject/Assets/ClientScripts/AndroidNative/AndroidNativeInterface.cs
@@ -126,7 +126,7 @@ public class ListenerAdapter : AndroidJavaProxy
     {
         Loom.QueueOnMainThread((param) =>
         {
-                    ViewUI.Instance.EditButton_clk();
+            ViewUI.Instance.EditButton_clk();
 
         }, null);
     }
@@ -136,7 +136,27 @@ public class ListenerAdapter : AndroidJavaProxy
         Loom.QueueOnMainThread((param) =>
                 {
                     ActiveScene.Instance.BackFirstpage();
-        
+
+
+                }, null);
+    }
+
+    void onFullScreen(bool b)
+    {
+        Loom.QueueOnMainThread((param) =>
+                {
+
+                    Screen.fullScreen = b;
+                    if (b)
+                    {
+                        BarHelper.Hide();
+
+                    }
+                    else
+                    {
+                        BarHelper.Show();
+
+                    }
 
                 }, null);
     }
@@ -202,5 +222,57 @@ public class AndroidNativeInterface : MonoBehaviour
     {
         return CallJavaFunc<string>("CallFromUnity", funcName, value);
 
+    }
+}
+
+
+
+public class BarHelper
+{
+    static int newStatusBarValue;
+    /// <summary>
+    ///  隐藏上方状态栏
+    /// </summary>
+    public static void Hide()
+    {
+#if !UNITY_EDITOR && UNITY_ANDROID
+        setStatusBarValue(1024); // WindowManager.LayoutParams.FLAG_FULLSCREEN; change this to 0 if unsatisfied
+#endif
+    }
+
+    /// <summary>
+    ///  显示上方状态栏
+    /// </summary>
+    public static void Show()
+    {
+#if !UNITY_EDITOR && UNITY_ANDROID
+        setStatusBarValue(2048); // WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN
+#endif
+    }
+
+    private static void setStatusBarValue(int value)
+    {
+        newStatusBarValue = value;
+        using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                activity.Call("runOnUiThread", new AndroidJavaRunnable(setStatusBarValueInThread));
+            }
+        }
+    }
+
+    private static void setStatusBarValueInThread()
+    {
+        using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                using (var window = activity.Call<AndroidJavaObject>("getWindow"))
+                {
+                    window.Call("setFlags", newStatusBarValue, newStatusBarValue);
+                }
+            }
+        }
     }
 }
